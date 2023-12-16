8d69a7b7c428123145c5028b109402adf52b5adb
Stilyana K. Stoyanova <skstoqnova@yahoo.com>
2023-12-16 19:13:33 +0100

    first add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemsListComponent } from './components/items-list/items-list.component';
+import { ItemDetailsComponent } from './pages/item-details/item-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'item-list', component: ItemsListComponent },
+  { path: 'item-details/:id', component: ItemDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
